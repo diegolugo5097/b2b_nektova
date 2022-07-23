200b1fcf6b758cd4f2b01a2242566ad9c900c8af
diegolugo5097 <vitaly818@hotmail.com>
2022-07-23 15:19:35 -0500

    refactor api and nuxt config in server-middleware

--- layouts/default.vue
+++ layouts/default.vue
@@ -128,7 +128,30 @@ export default {
     },
 
     signIn() {
-      fetchApi(options(this.username, this.password))
+      axios
+        .post(
+          'https://b2b.thecornercloud.com/oauth2-token',
+          {
+            grant_type: 'password',
+            client_id: 'dtGgEgUWUm5XImbmBcgCNmHL-n6pAhIh',
+            client_secret:
+              'LFZzIRsDXgsqJhYK8FDNKiXs3j7QMBRvrh5zsDVj83t5veIb2ac_IbXze7aZSUl4XNQ39d2sAasKSbzcpErnUo',
+            username: 'customer@example.com',
+            password: 'Custom4rD4mo*',
+          },
+          {
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'Content-Type': 'application/json',
+            },
+          }
+        )
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
     },
   },
 }
